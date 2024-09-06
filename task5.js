3761fc7ebe08a6aa1c52e5a43e06e613da3ede61
AyshaUrmi0 <ayshaismail021@gmail.com>
2024-09-06 23:23:48 +0900

    task5 done

---
+++ task5.js
@@ -0,0 +1,19 @@
+
+
+
+function odd_even(value){
+    if(value%2===0)
+    {
+        return "even";
+
+    }
+    else {
+        return "odd";
+    }
+
+}
+
+
+
+const value=odd_even(5);
+console.log(value);
