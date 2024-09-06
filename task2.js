8c0140752fce77f64f2fd6b903a0c771aef67d65
AyshaUrmi0 <ayshaismail021@gmail.com>
2024-09-06 22:51:38 +0900

    task 2 done

---
+++ task2.js
@@ -0,0 +1,12 @@
+// Task-2
+// Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+
+function doubleOrHalf(number) {
+    if(number % 2 === 0) {
+        return number / 2
+    } else {
+        return number * 2
+    }
+}
+
+console.log(doubleOrHalf(5))
